17237d1f7ea86f0747ac7a90709b5b9e0ce97d52
ranjumol <46959511+ranjumol@users.noreply.github.com>
2019-02-09 20:43:07 +0530

    Create rr5.c

---
+++ rr5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2,n3;
+clrscr();
+printf("enter the valuse");
+scanf("%d%d%d",&n1,&n2,&n3);
+if(n1>n2)
+{
+if(n1>n2)
+{
+printf(" greater");
+}
+else
+{
+printf("greater");
+}
+else if(n2>n3 )
+{printf("greater");
+else
+printf("greater");
+getch();
+}
