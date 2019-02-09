862c1db7119bf739473a77286fc434eb46357613
ranjumol <46959511+ranjumol@users.noreply.github.com>
2019-02-09 20:32:32 +0530

    Create rr3.c

---
+++ rr3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+int a,b;
+printf("enter alphabet");
+scanf("%c",&c);
+a=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+b=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+if(a||b)
+{
+printf("%c is a vowel",c);
+}
+else
+printf("%c is a consonant",c);
+getch();
+}
