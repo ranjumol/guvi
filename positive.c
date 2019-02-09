225b1d742440587e45e000d15e5d8afa15307bd7
ranjumol <46959511+ranjumol@users.noreply.github.com>
2019-02-09 20:24:07 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter a number");
+scanf("%d",&n);
+if(n>=0)
+{
+printf("positive");
+}
+else
+printf("negative");
+    getch();
+    }
