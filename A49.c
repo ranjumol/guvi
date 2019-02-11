5b22dca94b6203e0f549de0e335d890210c9b756
ranjumol <46959511+ranjumol@users.noreply.github.com>
2019-02-11 02:12:37 -0800

    Create A49.c

---
+++ A49.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+{
+	int a,b,c;
+	printf("enter the number:");
+	scanf("%d",&a);
+	printf("enter the number:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("%d",c);
+	return 0;
+}
