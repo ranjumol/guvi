5a682370f7936d8d1b7c42ce04fb92fe6e91b94d
ranjumol <46959511+ranjumol@users.noreply.github.com>
2019-02-11 19:33:43 +0530

    Create A62.c

---
+++ A62.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+char str[100];
+int i;
+printf("\n Enter the string");
+scanf("%s",&str);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]>='0'&&str[i]<='1')
+{
+printf("yes");
+break;
+}
+else
+{
+printf("no");
+break;
+}
+}
+}
