2b0b6584db9ff015dd60a9336b68c66db9d75d0d
ranjumol <46959511+ranjumol@users.noreply.github.com>
2019-02-11 02:55:58 -0800

    Create Aa70.c

---
+++ Aa70.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,i;
+printf("enter a number");
+scanf("%d",&a);
+for(i=0;i<n;i++)
+{
+b=pow(2,i);
+if(b>a)
+{
+c=i;
+break;
+}
+}
+d=pow(2,c);
+printf("\n%d",d);
+}
