c8c543a09a512de32969704cb75e1776d892a420
ranjumol <46959511+ranjumol@users.noreply.github.com>
2019-02-12 20:07:16 +0530

    Create A78.c

---
+++ A78.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if((n%13)==0)
+{
+printf("yes..num is multiple of 13");
+}
+else
+{
+printf("no..num is not a multiple of 13");
+}
+}
