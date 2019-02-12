507392e0deb652ec5c4278cf268dac2c3f2a51c5
ranjumol <46959511+ranjumol@users.noreply.github.com>
2019-02-12 21:24:59 +0530

    Create AA91.c

---
+++ AA91.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    float width,height,length;
+    float surarea,volume;
+    printf("enter the width,height and length\n");
+    scanf("%f %f %f",&length,&width,&height);
+    surarea=2*(length*height+length*width+width*height);
+    volume=length*width*height;
+    printf("the surface area of cuboid is=%.3f\n",surarea);
+    printf("the volume of cuboid is=%.3f\n",volume);
+}
