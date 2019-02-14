177f9913df0943baeec594a0a03a1c86d282a8ad
ranjumol <46959511+ranjumol@users.noreply.github.com>
2019-02-14 02:24:40 -0800

    Create A98.c

---
+++ A98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void) {
+	int a[3],i,j,max;
+	for(i=1;i<=4;i++)
+	scanf("%d",&a[i]);
+	for(i=1;i<=4;i++)
+	{
+	 for(j=i+1;j<=4;j++)
+	{
+	if(a[i]>a[j])
+	{
+	max=a[i];
+	printf("%d\n",i);
+	}
+	}
+	}
+return 0;
+}
+© 2019 GitHub, Inc.
