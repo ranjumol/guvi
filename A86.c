794726ea2ba271b75509af018f3b9339cadca69c
ranjumol <46959511+ranjumol@users.noreply.github.com>
2019-02-14 01:58:45 -0800

    Create A86.c

---
+++ A86.c
@@ -0,0 +1,24 @@
+#include<sdio.h>
+void main()
+{
+char s[20],s1[20],s2[20],j=0,k=0;
+int i=0;
+printf("\nEnter a sring");
+scanf("%s",s);
+while(s[i]!='\0')
+{
+if(i%2==0)
+{
+s1[j]=s[i];
+j++;
+}
+else
+{
+s2[k]=s[i];
+k++;
+}
+i++;
+}
+printf("\nOdd position sring:%s",s1);
+printf("\nEven Positon sring:%s",s2);
+}
